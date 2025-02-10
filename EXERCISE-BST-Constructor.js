b0595a176f0457aa0517a2098af15d908de93c1e
shruti <shrutiupari@gmail.com>
2025-02-10 15:24:16 +0530

    trees coding and understanding flow

---
+++ EXERCISE-BST-Constructor.js
@@ -0,0 +1,41 @@
+// WRITE NODE CLASS HERE //
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+ 
+class BST {
+	// WRITE BST CONSTRUCTOR HERE //
+	constructor() {
+        this.root = null
+    }
+}
+
+ 
+ 
+// function test() {
+//     let myBST = new BST();   
+    
+//     if (myBST.root === null) {
+//         console.log("Root: null");
+//     } else {
+//         console.log("Root:", myBST.root.value);
+//     }
+// }
+
+
+// test();
+
+let myBST = new BST()
+myBST
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Root: null
+
+*/
